f41b8f493d6ebb4d687ab2f66194af75899855b0
Mihail Prohorov <mwxood@users.noreply.github.com>
2021-06-01 19:12:05 +0300

    init

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,3 +1,16 @@
 <nav class="nav-holder">
-       
+   <div class="logo-holder">
+        <span class="nav-logo">
+            <?php include('includes/logo.php') ?> 
+        </span>
+        <span class="logo-text">Раредис Регистър</span>
+   </div>
+
+    <ul class="nav">
+        <li><a href="#">Начало</a></li>
+        <li><a href="#"><i class="fas fa-user-injured"></i>Пациенти</a></li>
+        <li><a href="#"><i class="far fa-address-card"></i>Регистрация на пациент</a></li>
+        <li><a href="#"><i class="fas fa-user-md"></i>Лекари</a></li>
+        <li><a href="#"><i class="fas fa-cog"></i>Настройки</a></li>
+    </ul>
 </nav>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php include('includes/header.php'); ?>
+   
     <main>
+        <div class="content-holder">
+            <?php include('includes/navigation.php') ?>
+            <div class="content">
+            
+            </div><!--end content-->
+        </div>
     </main>
 <?php include('includes/footer.php') ?>
